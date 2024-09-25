18d75f08a04c8b274476fe1401181f7b1da4a7e3
EdricJensenSee <125674757+EdricJensenSee@users.noreply.github.com>
2024-09-26 00:13:07 +0800

    Modified Home Style

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         <div className="absolute inset-0 bg-dark-green bg-opacity-50 flex flex-col items-center justify-center">
           <h1 className="text-white text-[96px] font-bold mb-4 italic text-center">Keep Your Home<br></br> Safe</h1>
           <div className="flex space-x-4 text-xl">
-            <button className="btn bg-normal-green hover:bg-dark-green text-white h-[56px] w-[202px] rounded">BOOK NOW</button>
-            <button className="btn btn-outline border-white hover:bg-transparent hover:text-normal-green text-normal-green h-[56px] w-[202px] rounded">CONTACT US</button>
+            <button className="btn bg-normal-green hover:bg-dark-green text-white h-[56px] w-[202px] rounded border-none">BOOK NOW</button>
+            <button className="btn btn-outline border-normal-green hover:border-dark-green hover:bg-transparent hover:text-white text-white h-[56px] w-[202px] rounded">CONTACT US</button>
           </div>
         </div>
       </div>
