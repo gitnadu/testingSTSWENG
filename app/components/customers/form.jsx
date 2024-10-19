f167b3ff7610bf4b4820dc36dad72040795165d1
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-10-19 09:05:59 +0800

    Add an input for types of services and a submission button and improve customer form design

--- app/components/customers/form.jsx
+++ app/components/customers/form.jsx
@@ -1,6 +1,6 @@
 function TextInput({name, label}) {
     return (
-        <div>
+        <div className="my-1">
             <label for={name} 
             className="text-dark-green">
                 {label}
@@ -13,7 +13,7 @@ function TextInput({name, label}) {
 
 function EmailInput({name, label}) {
     return (
-        <div>
+        <div className="my-1">
             <label for={name}
             className="text-dark-green">
                 {label}
@@ -26,11 +26,39 @@ function EmailInput({name, label}) {
 
 function TelNumInput({name, label, regex}) {
     return (
-        <div>
+        <div className="my-1">
             <label for={name}
             className="text-dark-green">{label}</label>
             <input type="tel" id={name} name={label} pattern={regex} 
             className="border-[1px] border-dark-green rounded-md" /> {/* Using the landline and cellphone PH phone number format. */}
+        </div>
+    );
+}
+
+function CheckboxInput({name, label, options}) {
+    const optionList = options.map((option, index) => (
+        <div key={index} className="flex flex-row self-center">
+          <input
+            type="checkbox"
+            id={`${name}-${option}`} // Unique ID for each checkbox
+            name={name} // Consistent name for form submission
+            value={option}
+            className="mx-2 my-1"
+          />
+          <label key={`${index}-label`} for={`${name}-${option}`}
+          className="text-dark-green">
+            {option}
+          </label>
+        </div>
+    ));
+
+    return (
+        <div className="my-1">
+            <p id={name}
+            className="text-dark-green">
+                {label}
+            </p>
+            {optionList}
         </div>
     );
 }
@@ -41,7 +69,7 @@ function DropDownInput({name, label, options}) {
     );
 
     return (
-        <div>
+        <div className="my-1">
             <label for={name}
             className="text-dark-green">
                 {label}
@@ -55,26 +83,36 @@ function DropDownInput({name, label, options}) {
     );
 }
 
+function SubmitButton() {
+    return (
+        <input type="submit" value="+ Add Client" disabled={true} 
+        className="text-white text-center bg-lime-500 border-[1px] rounded-md px-2 py-1 w-max self-end mt-[45%] mr-2"/> //Disabled.
+    );
+}
+
 export default function CustomerForm(/*props*/) {
     const statusOptions = ["Completed", "Ongoing", "Terminated", "Pending"];
     const typeOptions = ["Industrial", "Residential", "Commercial", "Service", "Retail", "Other"];
+    const serviceOptions = ["Hygenic Pest Control", "Termite Control", "Rodent Control"]
 
     return (
-        <div className="flex flex-col">
-            <h2 className="text-dark-green text-[20px] font-bold">Customer Information</h2>
-            <div className="border-2 flex flex-row p-2">
-                <div className="border-2 flex flex-col p-15"> {/* column 1 */}
+        <form className="flex flex-col p-5">
+            <h2 className="text-dark-green text-[20px] font-bold mb-1">Customer Information</h2>
+            <div className="border-2 flex flex-row">
+                <div className="border-2 mx-2 flex flex-col "> {/* column 1 */}
                     <TextInput name="client_name" label="Client Name" />
                     <TextInput name="contact_person" label="Contact Person" />
                     <EmailInput name="email_address" label="Email Address" />
                     <TextInput name="address" label="Address" />
+                    <CheckboxInput name="services" label="Services" options={serviceOptions} />
                 </div>
-                <div className="border-2 flex flex-col"> {/* column 2 */}
+                <div className="border-2 mx-2 flex flex-col gap-2"> {/* column 2 */}
                     <DropDownInput name="status" label="Status" options={statusOptions}/>
                     <DropDownInput name="type" label="Type" options={typeOptions} />
                     <TelNumInput name="contact_number" label="Contact Number" regex="^(09|\+639)\d{9}$|^(02|\+6302)\d{8}$" />
+                    <SubmitButton />
                 </div>
             </div>
-        </div>
+        </form>
     );
 }
