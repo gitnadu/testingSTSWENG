75176a1742663a86a7e86b58774992dc8879f35b
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-10-25 15:38:55 +0800

    Add client-side form input validation

--- app/components/customers/form.jsx
+++ app/components/customers/form.jsx
@@ -7,10 +7,9 @@ function ErrorMessage({message}) {
     );
 }
 
-function TextInput({name, label, onChange}) {
-    const error = true;
+function TextInput({name, label, onChange, error_msg}) {
+    const error = false;
     const error_style = "border-red-600 bg-red-100";
-    const error_message = "Please fill out this field.";
 
     return (
         <div className="my-1.5">
@@ -21,16 +20,16 @@ function TextInput({name, label, onChange}) {
             <input type="text" id={name} name={name} 
             data-test={`input_${name}`}
             onChange={onChange}
-            className={`border-[1px] ${error && error_style} border-dark-green rounded-md`}
+            className={`border-[1px] ${error_msg && error_style} border-dark-green rounded-md`}
             />
-            {error &&
-            <ErrorMessage message={error_message} />}
+            {error_msg &&
+            <ErrorMessage message={error_msg} />}
         </div>
     );
 }
 
 function EmailInput({name, label, onChange}) {
-    const error = true;
+    const error = false;
     const error_style = "border-red-600 bg-red-100";
     const error_message = "Please fill out this field.";
 
@@ -44,14 +43,14 @@ function EmailInput({name, label, onChange}) {
             data-test={`input_${name}`}
             onChange={onChange} 
             className={`border-[1px] ${error && error_style} border-dark-green rounded-md`}/>
-            {error &&
+            {error_msg &&
             <ErrorMessage message={error_message} />}
         </div>
     );
 }
 
 function TelNumInput({name, label, regex, onChange}) {
-    const error = true;
+    const error = false;
     const error_style = "border-red-600 bg-red-100";
     const error_message = "Please fill out this field.";
 
@@ -63,16 +62,15 @@ function TelNumInput({name, label, regex, onChange}) {
             data-test={`input_${name}`}
             onChange={onChange}
             className={`border-[1px] ${error && error_style} border-dark-green rounded-md`} /> {/* Using the landline and cellphone PH phone number format. */}
-            {error &&
+            {error_msg &&
             <ErrorMessage message={error_message} />}
         </div>
     );
 }
 
-function CheckboxInput({name, label, options}) {
-    const error = true;
+function CheckboxInput({name, label, options, onChange, error_msg}) {
+    const error = false;
     const error_style = "border-red-600 bg-red-100 hover:bg-red-50";
-    const error_message = "Please fill out this field.";
 
     const optionList = options.map((option, index) => (
         <div key={index} className="flex flex-row self-center">
@@ -94,10 +92,11 @@ function CheckboxInput({name, label, options}) {
             cursor-pointer  
             checked:bg-lime-500 
             checked:border-lime-500"*/
+            onChange={onChange}
             className={`form-checkbox mx-2 my-1 w-4 h-4
             cursor-pointer 
             border-3 rounded-sm ${
-            error &&
+            error_msg &&
             error_style
             }
             border-dark-green-A
@@ -122,16 +121,15 @@ function CheckboxInput({name, label, options}) {
                 {label}
             </p>
             {optionList}
-            {error &&
-            <ErrorMessage message={error_message} />}
+            {error_msg &&
+            <ErrorMessage message={error_msg} />}
         </div>
     );
 }
 
-function DropDownInput({name, label, options, onChange}) {
-    const error = true;
+function DropDownInput({name, label, options, onChange, error_msg}) {
+    const error = false;
     const error_style = "border-red-600 bg-red-100";
-    const error_message = "Please fill out this field.";
 
     const optionList = options.map((option, index) =>
         <option key={index + 1} value={option}>{option}</option>
@@ -147,12 +145,12 @@ function DropDownInput({name, label, options, onChange}) {
             <select name={name} id={name}
             data-test={`input_${name}`}
             onChange={onChange}
-            className={`border-[1px] ${error && error_style} border-dark-green rounded-md`}>
+            className={`border-[1px] ${error_msg && error_style} border-dark-green rounded-md`}>
                  <option disabled selected value>Select {name}</option>
                 {optionList}
             </select>
-            {error &&
-            <ErrorMessage message={error_message} />}
+            {error_msg &&
+            <ErrorMessage message={error_msg} />}
         </div>
     );
 }
@@ -167,50 +165,155 @@ function SubmitButton({ onClick }) {
 }
 
 export default function CustomerForm(/*props*/) {
-    //States store inputs.
-    const [clientNameObj, setClientName] = useState({value: "", error: false});
-    const [contactPersonObj, setContactPerson] = useState({value: "", error: false});
-    const [emailAddressObj, setEmailAddress] = useState({value: "", error: false});
-    const [addressObj, setAddress] = useState({value: "", error: false});
-    const [servicesObj, setServices] = useState({value: [], error: false});
-    const [statusObj, setStatus] = useState({value: "", error: false});
-    const [typeObj, setType] = useState({value: "", error: false});
-    const [contactNumberObj, setContactNumber] = useState({value: "", error: false});
-
     const statusOptions = ["Completed", "Ongoing", "Terminated", "Pending"];
     const typeOptions = ["Industrial", "Residential", "Commercial", "Service", "Retail", "Other"];
-    const serviceOptions = ["Hygenic Pest Control", "Termite Control", "Rodent Control"]
+    const serviceOptions = ["Hygenic Pest Control", "Termite Control", "Rodent Control"];
+
+    //States store inputs.
+    const [formData, setFormData] = useState({
+        client_name: "",
+        contact_person: "",
+        email_address: "",
+        address: "",
+        services: [],
+        status: "",
+        type: "",
+        contact_number: "",
+    });
+
+    const [errors, setErrors] = useState({
+        client_name: "",
+        contact_person: "",
+        email_address: "",
+        address: "",
+        services: "",
+        status: "",
+        type: "",
+        contact_number: "",
+    })
 
     const printInputs = () => {
-        console.log(clientNameObj);
-        console.log(contactPersonObj);
-        console.log(emailAddressObj);
-        console.log(addressObj);
-        //console.log(servicesObj);
-        console.log(statusObj);
-        console.log(typeObj);
-        console.log(contactNumberObj);
+        console.log(formData.client_name);
+        console.log(formData.contact_person);
+        console.log(formData.email_address);
+        console.log(formData.address);
+        console.log(formData.services);
+        console.log(formData.status);
+        console.log(formData.type);
+        console.log(formData.contact_number);
     };
+
+    const printErrorMessages = () => {
+        console.log(errors.client_name);
+        console.log(errors.contact_person);
+        console.log(errors.email_address);
+        console.log(errors.address);
+        console.log(errors.services);
+        console.log(errors.status);
+        console.log(errors.type);
+        console.log(errors.contact_number);
+    }
+
+    const handleChange = (e) => {
+        e.stopPropagation();
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    };
+
+    const handleCheckboxChange = (e) => {
+        e.stopPropagation();
+
+        let services = formData.services
+
+        if (e.target.checked && !(services.includes(e.target.value))) {
+            services.push(e.target.value)
+        }
+
+        if (!e.target.checked) {
+            services = services.filter(service => service !== e.target.value);
+        }
+
+        //console.log(new_services);
+
+        setFormData({
+           ...formData,
+           services: services
+        });
+    };
+
+    const handleSubmit = () => {
+        const new_errors = {
+            client_name: "",
+            contact_person: "",
+            email_address: "",
+            address: "",
+            services: "",
+            status: "",
+            type: "",
+            contact_number: "",
+        };
+
+        const email_regex = /^\S+@\S+\.\S+$/
+        if (!email_regex.test(formData.email_address)) {
+            console.log(`email_address: Please follow the proper email address format.`)
+            new_errors.email_address = "Please follow the proper email address format.";
+        }
+
+        const telephone_regex = /^(09|\+639)\d{9}$|^(02|\+6302)\d{8}$/;
+        if (!telephone_regex.test(formData.contact_number)) {
+            console.log(`contact_number: Please follow the following formats.`)
+            new_errors.contact_number = "Please follow the following formats.";
+        }
+
+        //Handles requirements:
+        for (let data in formData) {
+            if (data !== "services" && formData[data] === "")  {
+                console.log(`${data}: Please fill out this field.`);
+                new_errors[data] = "Please fill out this field.";
+            }
+        }
+
+        if (formData.services.length <= 0) {
+            console.log(`services: Please fill out this field.`);
+            new_errors.services = "Please fill out this field.";
+        }
+
+        //console.log(new_errors);
+        setErrors(new_errors);
+    }
 
     return (
         <div className="flex flex-col p-5">
             <h2 className="text-dark-green-A text-[20px] font-bold mb-1">Customer Information</h2>
             <div className="border-2 flex flex-row">
                 <div className="border-2 mx-2 flex flex-col "> {/* column 1 */}
-                    <TextInput name="client_name" label="Client Name" />
-                    <TextInput name="contact_person" label="Contact Person" />
-                    <EmailInput name="email_address" label="Email Address" />
-                    <TextInput name="address" label="Address" />
-                    <CheckboxInput name="services" label="Services" options={serviceOptions} />
+                    <TextInput name="client_name" label="Client Name" onChange={handleChange} 
+                    error_msg={errors.client_name} />
+                    <TextInput name="contact_person" label="Contact Person" onChange={handleChange} 
+                    error_msg={errors.contact_person} />
+                    <TextInput name="email_address" label="Email Address" onChange={handleChange} 
+                    error_msg={errors.email_address} />
+                    <TextInput name="address" label="Address" onChange={handleChange} 
+                    error_msg={errors.address} />
+                    <CheckboxInput name="services" label="Services" options={serviceOptions} onChange={handleCheckboxChange}
+                    error_msg={errors.services} />
                 </div>
                 <div className="border-2 mx-2 flex flex-col"> {/* column 2 */}
                     <div className="flex flex-col  h-[90%]">
-                        <DropDownInput name="status" label="Status" options={statusOptions}/>
-                        <DropDownInput name="type" label="Type" options={typeOptions} />
-                        <TelNumInput name="contact_number" label="Contact Number" regex="^(09|\+639)\d{9}$|^(02|\+6302)\d{8}$" />
+                        <DropDownInput name="status" label="Status" options={statusOptions} onChange={handleChange}
+                        error_msg={errors.status} />
+                        <DropDownInput name="type" label="Type" options={typeOptions} onChange={handleChange} 
+                        error_msg={errors.type} />
+                        <TextInput name="contact_number" label="Contact Number" onChange={handleChange} 
+                        error_msg={errors.contact_number} /> {/* Regex: ^(09|\+639)\d{9}$|^(02|\+6302)\d{8}$*/}
                     </div>
                     <div className="flex flex-col flex-end justify-end">
-                        <SubmitButton onClick={printInputs} />
+                        <SubmitButton onClick={() => {
+                            printInputs();
+                            handleSubmit();
+                        }} />
                     </div>
                 </div>
             </div>
