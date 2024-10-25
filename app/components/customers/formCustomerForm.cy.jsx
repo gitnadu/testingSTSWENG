b4bbe6a87675114cdda183493925f5231e9596c9
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-10-25 17:56:19 +0800

    Add new cases to the customer form unit script

--- app/components/customers/formCustomerForm.cy.jsx
+++ app/components/customers/formCustomerForm.cy.jsx
@@ -20,4 +20,65 @@ describe('<CustomerForm />', () => {
     cy.getByData("submit_button").click();
     //cy.log(cy.location());
   })
+
+  it('Fill in a contact number incorrectly.', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<CustomerForm />)
+
+    cy.getByData("input_client_name").type("Multi-Pest Services Corp.");
+    cy.getByData("input_contact_person").type("Norma B. Salazar");
+    cy.getByData("input_email_address").type("multipest14@yahoo.com");
+    cy.getByData("input_address").type("Mandaluyong City, NCR");
+    cy.getByData("input_status").select("Ongoing");
+    cy.getByData("input_type").select("Service");
+    cy.getByData("input_services_hygenic_pest_control").click();
+    cy.getByData("input_contact_number").type("6319693");
+    cy.getByData("submit_button").click();
+    cy.getByData("error_contact_number").should("exist").contains("Please follow one of the following formats:");
+  });
+
+  it('Fill in an email address incorrectly.', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<CustomerForm />)
+
+    cy.getByData("input_client_name").type("Multi-Pest Services Corp.");
+    cy.getByData("input_contact_person").type("Norma B. Salazar");
+    cy.getByData("input_email_address").type("multipest@");
+    cy.getByData("input_address").type("Mandaluyong City, NCR");
+    cy.getByData("input_status").select("Ongoing");
+    cy.getByData("input_type").select("Service");
+    cy.getByData("input_services_hygenic_pest_control").click();
+    cy.getByData("input_contact_number").type("0263196933");
+    cy.getByData("submit_button").click();
+    cy.getByData("error_email_address").should("exist").should("have.text", "Please enter a properly formatted email address.");
+  })
+
+  it("Fill in some of the fields.", () => {
+    cy.mount(<CustomerForm />);
+
+    cy.getByData("input_client_name").type("DLSU Laguna");
+    cy.getByData("input_address").type("Laguna Boulevard, LTI Spine Road, Barangays Biñan and Malamig, Biñan City, Laguna");
+    cy.getByData("input_type").select("Service");
+    cy.getByData("input_services_termite_control").click();
+    cy.getByData("input_services_rodent_control").click();
+    cy.getByData("input_contact_number").type("09199650046");
+    cy.getByData("submit_button").click();
+    cy.getByData("error_contact_person").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_email_address").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_status").should("exist").should("have.text","Please enter this field.");
+  });
+
+  it("Do not fill in all fields.", () => {
+    cy.mount(<CustomerForm />);
+
+    cy.getByData("submit_button").click();
+    cy.getByData("error_client_name").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_contact_person").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_email_address").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_address").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_services").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_status").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_type").should("exist").should("have.text","Please enter this field.");
+    cy.getByData("error_contact_number").should("exist").should("have.text","Please enter this field.");
+  });
 })
