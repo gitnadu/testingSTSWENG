ef505bfd8833f0aac4801dfd13f08f1f028cc9c6
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-10-25 15:43:49 +0800

    Move components used in a customer form to a file formComponents.jsx

---
+++ app/components/customers/formComponents.jsx
@@ -0,0 +1,163 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+function ErrorMessage({message}) {
+    return (
+        <p className="text-red-600 text-[85%] mt-1">{message}</p>
+    );
+}
+
+export function TextInput({name, label, onChange, error_msg}) {
+    const error = false;
+    const error_style = "border-red-600 bg-red-100";
+
+    return (
+        <div className="my-1.5">
+            <label for={name} 
+            className="text-dark-green-A">
+                {label}
+            </label>
+            <input type="text" id={name} name={name} 
+            data-test={`input_${name}`}
+            onChange={onChange}
+            className={`border-[1px] ${error_msg && error_style} border-dark-green rounded-md`}
+            />
+            {error_msg &&
+            <ErrorMessage message={error_msg} />}
+        </div>
+    );
+}
+
+export function EmailInput({name, label, onChange}) {
+    const error = false;
+    const error_style = "border-red-600 bg-red-100";
+    const error_message = "Please fill out this field.";
+
+    return (
+        <div className="my-1.5">
+            <label for={name}
+            className="text-dark-green-A">
+                {label}
+            </label>
+            <input type="email" id={name} name={name}
+            data-test={`input_${name}`}
+            onChange={onChange} 
+            className={`border-[1px] ${error && error_style} border-dark-green rounded-md`}/>
+            {error_msg &&
+            <ErrorMessage message={error_message} />}
+        </div>
+    );
+}
+
+export function TelNumInput({name, label, regex, onChange}) {
+    const error = false;
+    const error_style = "border-red-600 bg-red-100";
+    const error_message = "Please fill out this field.";
+
+    return (
+        <div className="my-1.5">
+            <label for={name}
+            className="text-dark-green-A">{label}</label>
+            <input type="tel" id={name} name={label} pattern={regex} 
+            data-test={`input_${name}`}
+            onChange={onChange}
+            className={`border-[1px] ${error && error_style} border-dark-green rounded-md`} /> {/* Using the landline and cellphone PH phone number format. */}
+            {error_msg &&
+            <ErrorMessage message={error_message} />}
+        </div>
+    );
+}
+
+export function CheckboxInput({name, label, options, onChange, error_msg}) {
+    const error = false;
+    const error_style = "border-red-600 bg-red-100 hover:bg-red-50";
+
+    const optionList = options.map((option, index) => (
+        <div key={index} className="flex flex-row self-center">
+            {/*
+            References for checkbox designs:
+            https://www.geeksforgeeks.org/how-to-style-checkboxes-in-tailwind-css/
+            https://www.npmjs.com/package/@tailwindcss/forms?activeTab=readme
+             */}
+            <input
+            type="checkbox"
+            id={`${name}-${option}`} // Unique ID for each checkbox
+            name={name} // Consistent name for form submission
+            value={option}
+            data-test={`input_${name}_${option.replaceAll(" ", "_").toLowerCase()}`}
+            /*className="mx-2 my-1 
+            peer relative appearance-none 
+            w-5 h-5 border 
+            rounded-md border-dark-green-A
+            cursor-pointer  
+            checked:bg-lime-500 
+            checked:border-lime-500"*/
+            onChange={onChange}
+            className={`form-checkbox mx-2 my-1 w-4 h-4
+            cursor-pointer 
+            border-3 rounded-sm ${
+            error_msg &&
+            error_style
+            }
+            border-dark-green-A
+            hover:bg-slate-100
+            focus:ring-0 focus:ring-offset-0
+            checked:focus:bg-lime-500 
+            checked:bg-lime-500 
+            checked:border-lime-500
+            checked:hover:bg-lime-400`}
+            />
+            <label key={`${index}-label`} for={`${name}-${option}`}
+            className="text-dark-green-A">
+                {option}
+            </label>
+        </div>
+    ));
+
+    return (
+        <div className="my-1.5">
+            <p id={name}
+            className="text-dark-green-A">
+                {label}
+            </p>
+            {optionList}
+            {error_msg &&
+            <ErrorMessage message={error_msg} />}
+        </div>
+    );
+}
+
+export function DropDownInput({name, label, options, onChange, error_msg}) {
+    const error = false;
+    const error_style = "border-red-600 bg-red-100";
+
+    const optionList = options.map((option, index) =>
+        <option key={index + 1} value={option}>{option}</option>
+    );
+
+    return (
+        <div className="my-1.5">
+            <label for={name}
+            className="text-dark-green-A">
+                {label}
+            </label> 
+            <br />
+            <select name={name} id={name}
+            data-test={`input_${name}`}
+            onChange={onChange}
+            className={`border-[1px] ${error_msg && error_style} border-dark-green rounded-md`}>
+                 <option disabled selected value>Select {name}</option>
+                {optionList}
+            </select>
+            {error_msg &&
+            <ErrorMessage message={error_msg} />}
+        </div>
+    );
+}
+
+export function SubmitButton({ onClick }) {
+    return (
+        <input type="submit" value="+ Add Client" disabled={false}
+        data-test="submit_button" 
+        onClick={onClick}
+        className="text-white text-center bg-lime-500 border-[1px] rounded-md px-2 py-1 w-max self-end mr-2"/> //Disabled.
+    );
+}
