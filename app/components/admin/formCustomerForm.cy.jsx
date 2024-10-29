465b28627623add5b4323cfaa45f17a42f422a84
AndreiPalonpon <andrei_palonpon@dlsu.edu.ph>
2024-10-29 18:15:06 +0800

    Reorganize files for customer form

--- app/components/admin/formCustomerForm.cy.jsx
+++ app/components/admin/formCustomerForm.cy.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'tailwindcss/tailwind.css'; //For CSS tailwind to be used in Cypress, import the CSS file.
-import CustomerForm from './form'
+import CustomerForm from './customerForm'
 
 
 
